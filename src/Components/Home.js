df0be7c86501c916c7e8a18d8e1955e14edb6d53
powellsl99 <powellsl99@gmail.com>
2020-10-30 15:15:26 -0400

    add and impliment Husky, Prettier, ESLint, and Material UI

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <Typography>Hello from Home.js</Typography>
+            </div>
+        );
+    }
+}
+
+export default Home;
