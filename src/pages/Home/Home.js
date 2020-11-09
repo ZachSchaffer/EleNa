0e1aff6c9181de3770b72eb14bbd65f1261c5bde
Zachary Schaffer <zachary.schaffer12@gmail.com>
2020-11-08 21:10:26 -0500

    Make home extend React.Component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Typography } from '@material-ui/core';
 
-export default class Home {
+export default class Home extends React.Component {
   render() {
     return (
       <div>
