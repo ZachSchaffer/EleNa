3aa03eee8c5170b3153c750427b8f15f6cfd6cf9
Goose <cpgusev@gmail.com>
2020-11-11 04:19:13 -0500

    Added google maps

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Typography } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
+
+ 
+
 
 export default class Home extends React.Component {
+
+  static defaultProps = {
+    center: {lat: 59.95, lng: 30.33},
+    zoom: 11
+  };
+
   render() {
     return (
       <div>
@@ -9,6 +19,15 @@ export default class Home extends React.Component {
           Primary
         </Button>
         <Typography>Hello from Home.js</Typography>
+
+       {/* Important! Always set the container height explicitly */}
+       <div>
+        <GoogleMapReact
+          style={{}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          />
+      </div>
       </div>
     );
   }
