bf6b80b4ce709d282804e87f67314964f8d57670
powellsl99 <powellsl99@gmail.com>
2020-11-14 12:33:50 -0500

    Update prettierrc

--- src/Components/AddressInput/AddressInput.js
+++ src/Components/AddressInput/AddressInput.js
@@ -4,151 +4,140 @@ import axios from 'axios';
 import PropTypes from 'prop-types';
 
 class AddressInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleFetchGeoData = this.handleFetchGeoData.bind(this);
-        this.getGeoDataURL = this.getGeoDataURL.bind(this);
-        this.getElevationURL = this.getElevationURL.bind(this);
-        this.state = {
-            address: '',
-            fetchError: false,
-            isLoading: true,
-            latitude: null,
-            longitude: null,
-            elevation: null,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.handleFetchGeoData = this.handleFetchGeoData.bind(this);
+    this.getGeoDataURL = this.getGeoDataURL.bind(this);
+    this.getElevationURL = this.getElevationURL.bind(this);
+    this.state = {
+      address: '',
+      fetchError: false,
+      isLoading: true,
+      latitude: null,
+      longitude: null,
+      elevation: null,
+    };
+  }
 
-    getElevationURL(lat, lng) {
-        return `http://open.mapquestapi.com/elevation/v1/profile?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&unit=f&shapeFormat=raw&latLngCollection=${lat},${lng}`;
-    }
+  getElevationURL(lat, lng) {
+    return `http://open.mapquestapi.com/elevation/v1/profile?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&unit=f&shapeFormat=raw&latLngCollection=${lat},${lng}`;
+  }
 
-    getGeoDataURL(address) {
-        return `http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&location=${address}`;
-    }
+  getGeoDataURL(address) {
+    return `http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST_API_KEY}&location=${address}`;
+  }
 
-    // Re-calls handleFetchGeoData only when shouldFetch changes (this is updated in the parent by incrementing it on button click)
-    componentDidUpdate(prevProps) {
-        if (this.props.shouldFetch !== prevProps.shouldFetch) {
-            this.handleFetchGeoData();
-        }
+  // Re-calls handleFetchGeoData only when shouldFetch changes (this is updated in the parent by incrementing it on button click)
+  componentDidUpdate(prevProps) {
+    if (this.props.shouldFetch !== prevProps.shouldFetch) {
+      this.handleFetchGeoData();
     }
+  }
 
-    // Fetches geo data based on an address, and then uses the latitude and longitude to fetch the elevation
-    handleFetchGeoData() {
-        this.setState({
-            isLoading: true,
-            fetchError: false,
-            latitude: null,
-            longitude: null,
-            elevation: null,
-        });
-        axios
-            .get(this.getGeoDataURL(this.state.address))
+  // Fetches geo data based on an address, and then uses the latitude and longitude to fetch the elevation
+  handleFetchGeoData() {
+    this.setState({
+      isLoading: true,
+      fetchError: false,
+      latitude: null,
+      longitude: null,
+      elevation: null,
+    });
+    axios
+      .get(this.getGeoDataURL(this.state.address))
+      .then((resp) => {
+        console.log('Response received');
+        console.log(resp);
+        try {
+          this.setState({
+            longitude: resp.data.results[0].locations[0].displayLatLng.lng,
+            latitude: resp.data.results[0].locations[0].displayLatLng.lat,
+          });
+          axios
+            .get(
+              this.getElevationURL(
+                resp.data.results[0].locations[0].displayLatLng.lat,
+                resp.data.results[0].locations[0].displayLatLng.lng
+              )
+            )
             .then((resp) => {
-                console.log('Response received');
-                console.log(resp);
-                try {
-                    this.setState({
-                        longitude:
-                            resp.data.results[0].locations[0].displayLatLng.lng,
-                        latitude:
-                            resp.data.results[0].locations[0].displayLatLng.lat,
-                    });
-                    axios
-                        .get(
-                            this.getElevationURL(
-                                resp.data.results[0].locations[0].displayLatLng
-                                    .lat,
-                                resp.data.results[0].locations[0].displayLatLng
-                                    .lng
-                            )
-                        )
-                        .then((resp) => {
-                            console.log('Response received');
-                            console.log(resp);
-                            try {
-                                this.setState({
-                                    elevation:
-                                        resp.data.elevationProfile[0].height,
-                                    isLoading: false,
-                                });
-                                this.props.setElevation(
-                                    resp.data.elevationProfile[0].height
-                                );
-                            } catch (error) {
-                                this.setState({
-                                    fetchError: true,
-                                    isLoading: false,
-                                });
-                                console.error(
-                                    `Error: ${error}. Error extracting elevation. Ensure a valid address was input`
-                                );
-                            }
-                        })
-                        .catch((err) => {
-                            console.error(
-                                `Fetch failed with error ${err.message}`
-                            );
-                            this.setState({
-                                fetchError: true,
-                                isLoading: false,
-                            });
-                        });
-                } catch (error) {
-                    this.setState({ fetchError: true, isLoading: false });
-                    console.error(
-                        `Error: ${error}. Error extracting latitude/longitude. Ensure a valid address was input`
-                    );
-                }
+              console.log('Response received');
+              console.log(resp);
+              try {
+                this.setState({
+                  elevation: resp.data.elevationProfile[0].height,
+                  isLoading: false,
+                });
+                this.props.setElevation(resp.data.elevationProfile[0].height);
+              } catch (error) {
+                this.setState({
+                  fetchError: true,
+                  isLoading: false,
+                });
+                console.error(
+                  `Error: ${error}. Error extracting elevation. Ensure a valid address was input`
+                );
+              }
             })
             .catch((err) => {
-                console.error(`Fetch failed with error ${err.message}`);
-                this.setState({ fetchError: true, isLoading: false });
+              console.error(`Fetch failed with error ${err.message}`);
+              this.setState({
+                fetchError: true,
+                isLoading: false,
+              });
             });
-    }
+        } catch (error) {
+          this.setState({ fetchError: true, isLoading: false });
+          console.error(
+            `Error: ${error}. Error extracting latitude/longitude. Ensure a valid address was input`
+          );
+        }
+      })
+      .catch((err) => {
+        console.error(`Fetch failed with error ${err.message}`);
+        this.setState({ fetchError: true, isLoading: false });
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                <FormControl>
-                    <InputLabel>{this.props.addressLabel}</InputLabel>
-                    <Input
-                        aria-describedby={this.props.addressLabel}
-                        onChange={(e) =>
-                            this.setState({ address: e.target.value })
-                        }
-                    />
-                </FormControl>
-                <br />
-                <br />
-                <Typography>
-                    Latitude:{' '}
-                    {this.state.fetchError || this.state.isLoading
-                        ? 'null'
-                        : this.state.latitude}
-                </Typography>
-                <Typography>
-                    Longitude:{' '}
-                    {this.state.fetchError || this.state.isLoading
-                        ? 'null'
-                        : this.state.longitude}
-                </Typography>
-                <Typography>
-                    Elevation:{' '}
-                    {this.state.fetchError || this.state.isLoading
-                        ? 'null'
-                        : `${this.state.elevation} ft`}
-                </Typography>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <FormControl>
+          <InputLabel>{this.props.addressLabel}</InputLabel>
+          <Input
+            aria-describedby={this.props.addressLabel}
+            onChange={(e) => this.setState({ address: e.target.value })}
+          />
+        </FormControl>
+        <br />
+        <br />
+        <Typography>
+          Latitude:{' '}
+          {this.state.fetchError || this.state.isLoading
+            ? 'null'
+            : this.state.latitude}
+        </Typography>
+        <Typography>
+          Longitude:{' '}
+          {this.state.fetchError || this.state.isLoading
+            ? 'null'
+            : this.state.longitude}
+        </Typography>
+        <Typography>
+          Elevation:{' '}
+          {this.state.fetchError || this.state.isLoading
+            ? 'null'
+            : `${this.state.elevation} ft`}
+        </Typography>
+      </div>
+    );
+  }
 }
 
 AddressInput.propTypes = {
-    shouldFetch: PropTypes.number,
-    setElevation: PropTypes.func,
-    addressLabel: PropTypes.string,
+  shouldFetch: PropTypes.number,
+  setElevation: PropTypes.func,
+  addressLabel: PropTypes.string,
 };
 
 export default AddressInput;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,54 +3,54 @@ import { Button } from '@material-ui/core';
 import AddressInput from '../../Components/AddressInput/AddressInput';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            shouldFetch: 0,
-            startElevation: null,
-            endElevation: null,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldFetch: 0,
+      startElevation: null,
+      endElevation: null,
+    };
+  }
 
-    setStartElevation(elevation) {
-        this.setState({ startElevation: elevation });
-    }
-    setEndElevation(elevation) {
-        this.setState({ endElevation: elevation });
-    }
+  setStartElevation(elevation) {
+    this.setState({ startElevation: elevation });
+  }
+  setEndElevation(elevation) {
+    this.setState({ endElevation: elevation });
+  }
 
-    render() {
-        return (
-            <div>
-                <AddressInput
-                    shouldFetch={this.state.shouldFetch}
-                    setElevation={this.setStartElevation.bind(this)}
-                    addressLabel="Starting Address"
-                    inputID="Starting Input"
-                />
-                <br />
-                <AddressInput
-                    shouldFetch={this.state.shouldFetch}
-                    setElevation={this.setEndElevation.bind(this)}
-                    addressLabel="Ending Address"
-                    inputID="Ending Input"
-                />
-                <br />
-                <Button
-                    onClick={() =>
-                        this.setState({
-                            shouldFetch: this.state.shouldFetch + 1,
-                        })
-                    }
-                    variant="contained"
-                    color="primary"
-                    aria-label="Fetch data for start and end address"
-                >
-                    Fetch Data
-                </Button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AddressInput
+          shouldFetch={this.state.shouldFetch}
+          setElevation={this.setStartElevation.bind(this)}
+          addressLabel="Starting Address"
+          inputID="Starting Input"
+        />
+        <br />
+        <AddressInput
+          shouldFetch={this.state.shouldFetch}
+          setElevation={this.setEndElevation.bind(this)}
+          addressLabel="Ending Address"
+          inputID="Ending Input"
+        />
+        <br />
+        <Button
+          onClick={() =>
+            this.setState({
+              shouldFetch: this.state.shouldFetch + 1,
+            })
+          }
+          variant="contained"
+          color="primary"
+          aria-label="Fetch data for start and end address"
+        >
+          Fetch Data
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default Home;
