9623f2784e14b55b5a2476b9c591f6c45024d77d
Caleb Coffin <coffincw@gmail.com>
2020-11-24 06:50:27 -0500

    Add comments and clean up code

--- src/Functions/Djikstra.js
+++ src/Functions/Djikstra.js
@@ -1,17 +1,14 @@
 import PriorityQueue from 'priorityqueue';
 export const FEET_IN_LAT_DEGREE = 364000;
 export const FEET_IN_LNG_DEGREE = 364434.53; // only for Amherst, MA
-export const MAX_ELEVATION = 10000;
-const MAX_DISTANCE = 1000000;
+const MAX_DISTANCE_OR_ELEVATION = 1000000;
 
 //This class runs the Djikstra algorithm on a grid of location objects passed from pathing service
 export class Djikstra {
     constructor(grid, startPosition, endPosition, height, width, toggle, x) {
         this.grid = grid;
-
         this.startPosition = startPosition;
         this.endPosition = endPosition;
-        // this.nodesList = nodesList;
         this.toggle = toggle;
         this.x = x;
         this.width = width;
@@ -23,6 +20,7 @@ export class Djikstra {
         this.getAdjacentPoints = this.getAdjacentPoints.bind(this);
         this.getPathDistance = this.getPathDistance.bind(this);
         this.determineShortestPathLength = this.determineShortestPathLength.bind(this);
+        this.isStartPosition = this.isStartPosition.bind(this);
     }
 
     //find the distance in feet between 2 location objects
@@ -53,6 +51,7 @@ export class Djikstra {
         return false;
     }
 
+    // Builds an array of [row, col] points that are adjacent to i, j
     getAdjacentPoints(i, j) {
         let points = [];
         if (i-1 >= 0) {
@@ -88,102 +87,33 @@ export class Djikstra {
         return points
     }
 
-    //create an adjacency matrix of the elevation gains between neighboring nodes to be used to find the path
+    //create an adjacency matrix to be used by Djikstra where each cell is a list of adjacent positions
     createAdjacencyMatrix() {
         let adjMatrix = [];
-        //calculate elevation gain between each pair of neighbors
         for (let i = 0; i < this.grid.length; i++) {
             adjMatrix.push([]);
             for (let j = 0; j < this.grid[i].length; j++) {
-                // holds the current locations elevation in meters
-                //let currElevation = this.grid[i][j].getElevation();
-
-                // holds the elevation gains for this location
-                //let elGains = [];
 
                 // add neighboring locations
                 adjMatrix[i].push([]);
                 adjMatrix[i][j] = this.getAdjacentPoints(i, j);
-                // this.getAdjacentPoints(i, j).forEach(point => {
-                //     adjMatrix[i][j].push(this.grid[point[0]][point[1]]);
-                // });
-                // iterate through all grid locations
-                // for (let k = 0; k < this.grid.length; k++) {
-                //     for (let l = 0; l < this.grid[k].length; l++) {
-
-                //         // Elevation gain from (i, j) to (k, 1)
-                //         let elevationGain = 0;
-
-                //         if (this.isAdjacent(k, l, i, j)) {
-                //             //if second point is higher, there is elevation gain, otherwise it is 0
-                //             let klElevation = this.grid[k][l].getElevation();
-                //             if (klElevation > currElevation) { 
-                //                 // if toggle is true then maximize elevation by doing 1/ gain
-                //                 elevationGain = this.toggle ? 1/(klElevation - currElevation) : klElevation - currElevation;
-                //             } else {
-                //                 // if toggle is true then maximize elevation by setting to 1 otherwise 0
-                //                 elevationGain = this.toggle ? 1 : 0;
-                //             }
-                //         }
-                //         else {
-                //             elevationGain = MAX_ELEVATION;
-                //         }
-                //         elGains.push(elevationGain);
-                //     }
-                //}
-                //adjMatrix.push(elGains);
 
             }
         }
-
-        // for (let j = 0; j < this.nodesList.length; j++) {
-        //   let currNode = this.nodesList[j].elevation;
-        //   let elevationDiff = [];
-        //   let elevationGain = 0;
-        //   for (let i = 0; i < this.nodesList.length; i++) {
-        //     //check if points are neighbors by their location in the grid
-        //     if (j === i - 1 || j === i + 1 || j === i + this.width || j === i + this.width - 1 || j === i + this.width + 1 || j === i - this.width || j === i - this.width - 1 || j === i - this.width + 1) {
-        //       // Take the elevation change, flip the sign depending on whether
-        //       // we're maximizing or minimizing
-        //       if (this.toggle) {
-        //         elevationGain = currNode - this.nodesList[i].elevation;
-        //       } else {
-        //         elevationGain = this.nodesList[i].elevation - currNode;
-        //       }
-        //     //   if (this.nodesList[i].elevation - currNode > 0) {
-        //     //     elevationGain = this.nodesList[i].elevation - currNode;
-        //     //   } else {
-        //     //     elevationGain = 0;
-        //     //   }
-        //     }
-        //     //if they are not neighbors, set edge weight to MAX_ELEVATION so we never traverse that edge
-        //     else {
-        //       elevationGain = MAX_ELEVATION;
-        //     }
-        //     elevationDiff.push(elevationGain);
-        //   }
-        //   adjMatrix.push(elevationDiff);
-        // }
-        //if we want to maximize instead of minimize, set elevation to 1/elevation
-        // if (this.toggle) {
-        //     for (let j = 0; j < this.nodesList.length; j++) {
-        //         for (let i = 0; i < this.nodesList.length; i++) {
-        //             if (adjMatrix[i][j] !== 0 && adjMatrix[i][j] !== MAX_ELEVATION) {
-        //                 adjMatrix[i][j] = 1 / adjMatrix[i][j];
-        //             }
-        //         }
-        //     }
-        // }
-        console.log(adjMatrix);
         return adjMatrix;
     }
 
-    // retrieves the path distance
+    // retrieves the path distance using the nodes list from the start position
+    // with the starting distance valuen of 'startDistance'
     getPathDistance(nodes, startPosition, startDistance) {
         let distance = startDistance;
         let curr = startPosition;
         let next = nodes[startPosition[0]][startPosition[1]];
+
+        // iterate till there are no more nodes on the path
         while (next !== null) {
+
+            // adds the distance between the 'curr' position and the 'next' position
             distance += this.distance(this.grid[next[0]][next[1]], this.grid[curr[0]][curr[1]]);
             curr = next;
             next = nodes[next[0]][next[1]];
@@ -191,34 +121,53 @@ export class Djikstra {
         return distance;
     }
 
+    // Checks if i, j is the start position
+    isStartPosition(i, j) {
+        return this.startPosition[0] === i && this.startPosition[1] === j;
+    }
+
+    // Runs traditional djikstras on the generated adjMatrix and grid to determine what the baseline
+    // for the k% elevation algorithm should be
     determineShortestPathLength(adjMatrix) {
-        let distances = []; //shortest distances to each node
+        let distances = []; // matrix of shortest distances to each node
         let pq = new PriorityQueue((this.height * this.width) ** 2);
         pq.enqueue(this.startPosition, 0);
+
+        // populate distances matrix with all values as MAX_DISTANCE_OR_ELEVATION 
+        // except 0 for the startPosition
         for (let i = 0 ; i < this.grid.length ; i ++) {
             distances.push([]);
             for (let j = 0 ; j < this.grid[i].length ; j++) {
-                if (this.startPosition[0] === i && this.startPosition[1] === j) {
+                if (this.isStartPosition(i, j)) {
                     distances[i].push(0);
                 } else {
-                    distances[i].push(MAX_DISTANCE);
+                    distances[i].push(MAX_DISTANCE_OR_ELEVATION);
                 }
                 
             }
         }
 
+        // run Djikstras
         while (!pq.isEmpty()) {
             let position = pq.dequeue();
             let curr = this.grid[position[0]][position[1]];
+
+            // iterate over all neighbors
             adjMatrix[position[0]][position[1]].forEach(neighborPosition => {
                 let neighbor = this.grid[neighborPosition[0]][neighborPosition[1]];
+
+                // calculate new alternative distance
                 let newDistance = distances[position[0]][position[1]] + this.distance(curr, neighbor);
+
+                // if distance is shorter than current shortest distance
                 if (newDistance < distances[neighborPosition[0]][neighborPosition[1]]) {
                     distances[neighborPosition[0]][neighborPosition[1]] = newDistance;
                     pq.enqueue(neighborPosition, distances[neighborPosition[0]][neighborPosition[1]]);
                 }
             });
         }
+
+        // return the distance to the end position from the start position
         return distances[this.endPosition[0]][this.endPosition[1]];
     }
 
@@ -226,77 +175,68 @@ export class Djikstra {
     //at each sep check to make sure distance within x%
     //if exceeds x%, set distance to that node to infinity in matrix and visited to true
     determinePath(adjMatrix) {
+
         //find the shortest distance and distance within x% of that
-        let startLocation = this.grid[this.startPosition[0]][this.startPosition[1]];
-        let endLocation = this.grid[this.endPosition[0]][this.endPosition[1]];
-        // let shortestDistance = this.distance(
-        //     startLocation,
-        //     endLocation
-        // );
+        // run Djikstras using just distances to find the shortest path
         let shortestDistance = this.determineShortestPathLength(adjMatrix);
+
+        // calculate the buffer range to accept paths within
         let distancePlusX = shortestDistance * (1 + this.x / 100);
         let path = []; //the final path
-        
-        //let pathToNode = []; //track previous visited node to get to current node
-        //push distances from start to all other nodes to begin with
-        // for (let i = 0 ; i < this.grid.length ; i++) {
-        //     for (let j = 0 ; j < this.grid[i].length ; j++) {
-        //         pathToNode.push(null);
-        //         distances.push({
-        //             index: [i,j],
-        //             node: this.grid[i][j],
-        //             dist: adjMatrix[0][(i*j)+j],
-        //             visited: (i===0 && j ===0) || this.grid[i][j].elevation === null,
-        //         })
-        //     }
-        // }
 
-        // for (let j = 0; j < this.nodesList.length; j++) {
-        //     pathToNode.push(null);
-        //     distances.push({
-        //         num: j,
-        //         node: this.nodesList[j],
-        //         dist: adjMatrix[0][j],
-        //         visited: j === 0 || this.nodesList[j].elevation === null,
-        //     });
-        // }
-        // let currNode = 0; //node that we are at now
-        // let pathSoFar = 0;  //distance travelled so far
-
-        let prevNodes = [];
+        let prevNodes = []; //list of previous nodes, used to backtrack to retrieve the path
         let distances = []; //shortest distances to each node
         let pq = new PriorityQueue((this.height * this.width) ** 2);
         pq.enqueue(this.startPosition, 0);
+
+        // populate distances matrix
         for (let i = 0 ; i < this.grid.length ; i ++) {
             distances.push([]);
             prevNodes.push([]);
             for (let j = 0 ; j < this.grid[i].length ; j++) {
-                if (this.startPosition[0] === i && this.startPosition[1] === j) {
+
+                // put MAX_DISTANCE_OR_ELEVATION in every cell except for the startPosition
+                // put 0 in the startPosition
+                if (this.isStartPosition(i, j)) {
                     distances[i].push(0);
                 } else {
-                    distances[i].push(MAX_ELEVATION);
+                    distances[i].push(MAX_DISTANCE_OR_ELEVATION);
                 }
                 prevNodes[i].push(null);
                 
             }
         }
 
+        // end location object
+        let endLocation = this.grid[this.endPosition[0]][this.endPosition[1]];
+
+        // run Djikstras
         while (!pq.isEmpty()) {
             let position = pq.dequeue();
             let currElevation = this.grid[position[0]][position[1]].getElevation();
+
+            // iterate through all of the location at 'position's neighbors
             adjMatrix[position[0]][position[1]].forEach(neighborPosition => {
                 let neighbor = this.grid[neighborPosition[0]][neighborPosition[1]];
                 let newElevation = distances[position[0]][position[1]];
 
+                // add the elevation gain
+                // divide 1/ gain if we want to maximize the elevation gain, don't divide otherwise
+                // this.toggle denotes whether the elevation gain should be maximized
                 if (neighbor.getElevation() > currElevation) {
                     newElevation += this.toggle ? 1/(neighbor.getElevation() - currElevation) : neighbor.getElevation() - currElevation;
                 } else {
                     newElevation += this.toggle ? 1 : 0;
                 }
 
+                // calculates the distance from this neighbor to the end location
                 let distanceToDestination = this.distance(neighbor, endLocation);
+
+                // calculates the current distance on the path from the start location to the neighbor
                 let currDistance = this.getPathDistance(prevNodes, position, this.distance(neighbor, this.grid[position[0]][position[1]]));
 
+                // checks if we should continue down this path
+                // only continue if the elevation is optimal and the path is within k%
                 if (newElevation < distances[neighborPosition[0]][neighborPosition[1]] && currDistance + distanceToDestination < distancePlusX) {
                     distances[neighborPosition[0]][neighborPosition[1]] = newElevation;
                     prevNodes[neighborPosition[0]][neighborPosition[1]] = position;
@@ -305,69 +245,15 @@ export class Djikstra {
             });
         }
 
-
-        //while we havent found the target node
-        // while (pathToNode[this.nodesList.length - 1] === null) {
-        //     let minDistance = MAX_ELEVATION;
-        //     let closestNode = null;
-        //     //find the node with the smallest total distance
-        //     for (let j = this.nodesList.length - 1; j >= 0; j--) {
-        //         if (!distances[j].visited && distances[j].dist < minDistance) {
-        //             minDistance = distances[j].dist;
-        //             closestNode = distances[j].num;
-        //         }
-        //     }
-        //     if (closestNode === null) {
-        //         pathToNode[this.nodesList.length - 1] = currNode;
-        //         break;
-        //     }
-        //     //update total distance travelled so far
-        //     pathSoFar += this.distance(
-        //         this.nodesList[currNode],
-        //         this.nodesList[closestNode]
-        //     );
-        //     //if path is not already longer than shortest path + x%, visit that node
-        //     //otherwise set node to visited, but do not make it the currNode
-        //     if (pathSoFar < distancePlusX || closestNode === this.nodesList.length - 1) {
-        //         pathToNode[closestNode] = currNode;
-        //         currNode = closestNode;
-        //         distances[currNode].visited = true;
-        //         distances[currNode].dist = minDistance;
-        //     } else {
-        //         distances[closestNode].visited = true;
-        //         distances[closestNode].dist = MAX_ELEVATION;
-        //     }
-        //     //update shortest paths if needed
-        //     for (let j = 0; j < this.nodesList.length; j++) {
-        //         if (
-        //             distances[j].dist >
-        //             distances[currNode].dist + adjMatrix[currNode][j]
-        //         ) {
-        //             //if path is not already too long
-        //             if (pathSoFar < distancePlusX) {
-        //                 distances[j].dist =
-        //                     distances[currNode].dist + adjMatrix[currNode][j];
-        //             }
-        //         }
-        //     }
-        // }
-        // console.log(pathToNode);
+        // retrieve the path from the prevNodes list by backtracking up the position stream
         path.push(endLocation);
         let next = prevNodes[this.endPosition[0]][this.endPosition[1]];
         while (next !== null) {
             path.push(this.grid[next[0]][next[1]]);
             next = prevNodes[next[0]][next[1]];
         }
-        // //if we have found the target node, find the path to that node
-        // if (pathToNode[this.nodesList.length - 1] !== null) {
-        //     path.push(this.nodesList[this.nodesList.length - 1]);
-        //     let next = pathToNode[this.nodesList.length - 1];
-        //     while (next !== 0) {
-        //         path.push(this.nodesList[next]);
-        //         next = pathToNode[next];
-        //     }
-        //     path.push(this.nodesList[0]);
-        // }
+
+        // reverse the path to get the start -> end location path needed by the UI
         console.log(path.reverse());
         return path.reverse();
     }
