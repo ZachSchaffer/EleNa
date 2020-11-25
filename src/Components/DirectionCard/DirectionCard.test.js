5e36594b499c49df42051bf937c2586f6f5d9171
powellsl99 <powellsl99@gmail.com>
2020-11-24 22:43:58 -0500

    merge main and remove package lock

--- src/Components/DirectionCard/DirectionCard.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import DirectionCard from './DirectionCard';
-
-test('test DirectionCard', () => {
-    const tree = renderer.create(<DirectionCard />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
